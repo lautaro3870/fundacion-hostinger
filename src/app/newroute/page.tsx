74565debb960482ac7777fc372c7979afa3d637b
lautaro3870 <lautarodisbro@gmail.com>
2024-09-26 18:14:26 +0200

    fetch data star wars

--- src/app/newroute/page.tsx
+++ src/app/newroute/page.tsx
@@ -1,7 +1,28 @@
+"use client"
+import { useEffect, useState } from "react";
+
 export default function NewRoute() {
+  const [data, setData] = useState([]);
+
+  const getData = async() => {
+    const response = await fetch("https://swapi.dev/api/people");
+    const dataResponse = await response.json();
+    setData(dataResponse.results);
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div>
       <h1>New Route</h1>
+      <br />
+      <ul>
+        {data.map((item: any, index: number) => (
+          <li key={index}>{item?.name} - {item?.hair_color}</li>
+        ))}
+      </ul>
     </div>
   );
 }
